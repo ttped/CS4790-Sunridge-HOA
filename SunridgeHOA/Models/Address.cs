d8136ae7b9752bc106095944c788e5c3169f708c
Sean Bigler <seanbigler@mail.weber.edu>
2019-02-25 11:31:29 -0700

    Started on models from ERD.

---
+++ SunridgeHOA/Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunridgeHOA.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public bool IsArchive { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
---
+++ SunridgeHOA/Models/OwnerHistory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunridgeHOA.Models
+{
+    public class OwnerHistory
+    {
+        public int OwnerHistoryId { get; set; }
+        public int LogId { get; set; }
+        public int OwnerId { get; set; }
+        public int HistoryTypeId { get; set; }
+
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string PrivacyLevel { get; set; }
+        public bool IsArchive { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+
+        //Navigation Properties
+        public virtual Lot Lot { get; set; }
+        public virtual Owner Owner { get; set; }
+        public virtual HistoryType HistoryType { get; set; }
+        public virtual ICollection<File> Files { get; set; }
+    }
+}
