27c773dd8fd479347c5d16db22b1acaa80d8025b
Jess <jessbrunker@gmail.com>
2019-04-06 19:59:25 -0600

    Let users edit own information

---
+++ SunridgeHOA/Areas/Owner/Models/ViewModels/OwnerInfoVM.cs
@@ -0,0 +1,14 @@
+﻿using SunridgeHOA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunridgeHOA.Areas.Owner.Models.ViewModels
+{
+    public class OwnerInfoVM
+    {
+        public SunridgeHOA.Models.Owner Owner { get; set; }
+        public Address Address { get; set; }
+    }
+}
