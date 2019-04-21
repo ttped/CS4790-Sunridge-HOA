bbf7fa942feeae210a81ce064e46e39b8198bc9e
Jess <jessbrunker@gmail.com>
2019-04-21 16:37:49 -0600

    Add migration for annotations (again)

---
+++ SunridgeHOA/Migrations/20190421223638_AddMoreAnnotations.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SunridgeHOA.Migrations
+{
+    public partial class AddMoreAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Photo",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Photo",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SerialNumber",
+                table: "Key",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Photo",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Photo",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SerialNumber",
+                table: "Key",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+        }
+    }
+}
