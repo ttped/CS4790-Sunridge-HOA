e6e2ae8f75b73354af781c513b240d3a89a17c22
Jess <jessbrunker@gmail.com>
2019-04-21 12:06:52 -0600

    Add migration for annotations

---
+++ SunridgeHOA/Migrations/20190421180439_AddAnnotations.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SunridgeHOA.Migrations
+{
+    public partial class AddAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Header",
+                table: "NewsItem",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "NewsItem",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ItemName",
+                table: "ClassifiedListing",
+                maxLength: 75,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ClassifiedListing",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Header",
+                table: "NewsItem",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "NewsItem",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ItemName",
+                table: "ClassifiedListing",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 75);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ClassifiedListing",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000);
+        }
+    }
+}
