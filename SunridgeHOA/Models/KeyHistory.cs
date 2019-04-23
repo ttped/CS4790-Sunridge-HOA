fe1e8ef6e32c26ddeba72f6c8b84a7d8e661db36
goeunk <goeunkwak22@gmail.com>
2019-04-23 12:35:50 -0600

    KeyHistory.cs

--- SunridgeHOA/Models/KeyHistory.cs
+++ SunridgeHOA/Models/KeyHistory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +14,7 @@ namespace SunridgeHOA.Models
         public int OwnerId { get; set; }
 
         public string Status { get; set; }
+        [Display(Name="Date Issued")]
         public DateTime DateIssued { get; set; }
         public DateTime DateReturned { get; set; }
         public float PaidAmount { get; set; }
