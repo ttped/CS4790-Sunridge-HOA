af62004a315db0f802aaf24d761fe8ad6e294f2b
Jess <jessbrunker@gmail.com>
2019-04-29 20:39:47 -0600

    Remove HOA Documents

--- SunridgeHOA/Areas/Admin/Controllers/HOADocsController.cs
+++ SunridgeHOA/Areas/Admin/Controllers/HOADocsController.cs
@@ -30,118 +30,118 @@ namespace SunridgeHOA.Areas.Admin.Controllers
             _hostingEnv = env;
         }
 
-        public async Task<IActionResult> Index(string query)
-        {
-            var identityUser = await _userManager.GetUserAsync(HttpContext.User);
-            var roles = await _userManager.GetRolesAsync(identityUser);
-            var isAdmin = roles.Contains("Admin") || roles.Contains("SuperAdmin");
+        //public async Task<IActionResult> Index(string query)
+        //{
+        //    var identityUser = await _userManager.GetUserAsync(HttpContext.User);
+        //    var roles = await _userManager.GetRolesAsync(identityUser);
+        //    var isAdmin = roles.Contains("Admin") || roles.Contains("SuperAdmin");
 
-            var hoa = _context.Lot.SingleOrDefault(u => u.LotNumber == "HOA");
+        //    var hoa = _context.Lot.SingleOrDefault(u => u.LotNumber == "HOA");
 
-            var filesQuery = _context.LotHistory
-                .Include(u => u.Files)
-                .Include(u => u.HistoryType)
-                .Where(u => u.LotId == hoa.LotId);
-            if (!isAdmin)
-            {
-                filesQuery = filesQuery.Where(u => u.PrivacyLevel != "Admin");
-            }
+        //    var filesQuery = _context.LotHistory
+        //        .Include(u => u.Files)
+        //        .Include(u => u.HistoryType)
+        //        .Where(u => u.LotId == hoa.LotId);
+        //    if (!isAdmin)
+        //    {
+        //        filesQuery = filesQuery.Where(u => u.PrivacyLevel != "Admin");
+        //    }
 
-            var files = await filesQuery.ToListAsync();
+        //    var files = await filesQuery.ToListAsync();
 
-            ViewData["LotId"] = hoa.LotId;
-            return View(files);
-        }
+        //    ViewData["LotId"] = hoa.LotId;
+        //    return View(files);
+        //}
 
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AddDocument()
-        {
-            var lot = _context.Lot.Single(u => u.LotNumber == "HOA");
-            //if (lot == null)
-            //{
-            //    return NotFound();
-            //}
+        //[Authorize(Roles = "Admin")]
+        //public async Task<IActionResult> AddDocument()
+        //{
+        //    var lot = _context.Lot.Single(u => u.LotNumber == "HOA");
+        //    //if (lot == null)
+        //    //{
+        //    //    return NotFound();
+        //    //}
 
-            var identityUser = await _userManager.GetUserAsync(HttpContext.User);
-            var roles = await _userManager.GetRolesAsync(identityUser);
-            var isAdmin = roles.Contains("Admin") || roles.Contains("SuperAdmin");
+        //    var identityUser = await _userManager.GetUserAsync(HttpContext.User);
+        //    var roles = await _userManager.GetRolesAsync(identityUser);
+        //    var isAdmin = roles.Contains("Admin") || roles.Contains("SuperAdmin");
 
-            if (!isAdmin)
-            {
-                return NotFound();
-            }
+        //    if (!isAdmin)
+        //    {
+        //        return NotFound();
+        //    }
 
-            ViewData["HistoryTypes"] = new SelectList(_context.HistoryType, "HistoryTypeId", "Description");
+        //    ViewData["HistoryTypes"] = new SelectList(_context.HistoryType, "HistoryTypeId", "Description");
 
-            return View(new DocumentVM
-            {
-                Id = lot.LotId
-            });
-        }
+        //    return View(new DocumentVM
+        //    {
+        //        Id = lot.LotId
+        //    });
+        //}
 
-        [Authorize(Roles = "Admin")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> AddDocument(DocumentVM vm)
-        {
-            var identityUser = await _userManager.GetUserAsync(HttpContext.User);
-            var loggedInUser = _context.Owner.Find(identityUser.OwnerId);
+        //[Authorize(Roles = "Admin")]
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> AddDocument(DocumentVM vm)
+        //{
+        //    var identityUser = await _userManager.GetUserAsync(HttpContext.User);
+        //    var loggedInUser = _context.Owner.Find(identityUser.OwnerId);
 
-            var lot = _context.Lot.Single(u => u.LotNumber == "HOA");
+        //    var lot = _context.Lot.Single(u => u.LotNumber == "HOA");
 
-            var files = HttpContext.Request.Form.Files;
-            if (files.Count == 0)
-            {
-                ModelState.AddModelError("Files", "Please upload at least one file");
-            }
+        //    var files = HttpContext.Request.Form.Files;
+        //    if (files.Count == 0)
+        //    {
+        //        ModelState.AddModelError("Files", "Please upload at least one file");
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                var uploadFiles = new List<SunridgeHOA.Models.File>();
-                foreach (var file in files)
-                {
-                    var webRootPath = _hostingEnv.WebRootPath;
-                    var folder = SD.LotDocsFolder;
-                    var uploads = Path.Combine(webRootPath, folder);
-                    var name = Path.GetFileNameWithoutExtension(file.FileName);
-                    var extension = Path.GetExtension(file.FileName);
-                    var dateExt = DateTime.Now.ToString("MMddyyyy");
-                    var newFileName = $"{lot.LotNumber} - {name} {dateExt}{extension}";
+        //    if (ModelState.IsValid)
+        //    {
+        //        var uploadFiles = new List<SunridgeHOA.Models.File>();
+        //        foreach (var file in files)
+        //        {
+        //            var webRootPath = _hostingEnv.WebRootPath;
+        //            var folder = SD.LotDocsFolder;
+        //            var uploads = Path.Combine(webRootPath, folder);
+        //            var name = Path.GetFileNameWithoutExtension(file.FileName);
+        //            var extension = Path.GetExtension(file.FileName);
+        //            var dateExt = DateTime.Now.ToString("MMddyyyy");
+        //            var newFileName = $"{lot.LotNumber} - {name} {dateExt}{extension}";
 
-                    using (var filestream = new FileStream(Path.Combine(uploads, newFileName), FileMode.Create))
-                    {
-                        file.CopyTo(filestream);
-                    }
+        //            using (var filestream = new FileStream(Path.Combine(uploads, newFileName), FileMode.Create))
+        //            {
+        //                file.CopyTo(filestream);
+        //            }
 
-                    var uploadFile = new SunridgeHOA.Models.File
-                    {
-                        FileURL = $@"\{folder}\{newFileName}",
-                        Date = DateTime.Now,
-                        Description = Path.GetFileName(file.FileName)
-                    };
-                    _context.File.Add(uploadFile);
-                    uploadFiles.Add(uploadFile);
-                    //await _context.SaveChangesAsync();
-                }
+        //            var uploadFile = new SunridgeHOA.Models.File
+        //            {
+        //                FileURL = $@"\{folder}\{newFileName}",
+        //                Date = DateTime.Now,
+        //                Description = Path.GetFileName(file.FileName)
+        //            };
+        //            _context.File.Add(uploadFile);
+        //            uploadFiles.Add(uploadFile);
+        //            //await _context.SaveChangesAsync();
+        //        }
 
-                var lotHistory = new LotHistory
-                {
-                    LotId = lot.LotId,
-                    HistoryTypeId = vm.HistoryType,
-                    Date = DateTime.Now,
-                    Description = vm.Description,
-                    PrivacyLevel = vm.AdminOnly ? "Admin" : "Owner",
-                    LastModifiedBy = loggedInUser.FullName,
-                    LastModifiedDate = DateTime.Now,
-                    Files = uploadFiles
-                };
-                _context.LotHistory.Add(lotHistory);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
+        //        var lotHistory = new LotHistory
+        //        {
+        //            LotId = lot.LotId,
+        //            HistoryTypeId = vm.HistoryType,
+        //            Date = DateTime.Now,
+        //            Description = vm.Description,
+        //            PrivacyLevel = vm.AdminOnly ? "Admin" : "Owner",
+        //            LastModifiedBy = loggedInUser.FullName,
+        //            LastModifiedDate = DateTime.Now,
+        //            Files = uploadFiles
+        //        };
+        //        _context.LotHistory.Add(lotHistory);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
 
-            ViewData["HistoryTypes"] = new SelectList(_context.HistoryType, "HistoryTypeId", "Description", vm.HistoryType);
-            return View(vm);
-        }
+        //    ViewData["HistoryTypes"] = new SelectList(_context.HistoryType, "HistoryTypeId", "Description", vm.HistoryType);
+        //    return View(vm);
+        //}
     }
 }
